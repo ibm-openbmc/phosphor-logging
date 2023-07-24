50b517daa9bd84986757de6e8d08faf3594518f4
Reed Frandsen <rfrandse@us.ibm.com>
2023-07-24 15:34:01 -0500

    PEL: Support for CheckstopFlag msg reg field (#30)
    * PEL: Support for CheckstopFlag msg reg field
    
    Similiar to the DeconfigFlag field that was recently added, this one
    indicates the PEL is for a hardware checkstop and results in a bit in
    SRC hex word 5 being set.
    
    
    Change-Id: Ib05de7471ad3e32f48e7f20a5c611abc119fe82a
    
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- test/openpower-pels/src_test.cpp
+++ test/openpower-pels/src_test.cpp
@@ -491,6 +491,7 @@ TEST_F(SRCTest, RegistryCalloutTest)
     message::Entry entry;
     entry.src.type = 0xBD;
     entry.src.reasonCode = 0xABCD;
+    entry.src.checkstopFlag = true;
     entry.subsystem = 0x42;
 
     entry.callouts = R"(
@@ -554,6 +555,10 @@ TEST_F(SRCTest, RegistryCalloutTest)
             .WillOnce(Return(std::vector<bool>{false, false, false}));
 
         SRC src{entry, ad, dataIface};
+
+        const auto& hexwords = src.hexwordData();
+        auto mask = static_cast<uint32_t>(SRC::ErrorStatusFlags::hwCheckstop);
+        EXPECT_EQ(hexwords[5 - 2] & mask, mask);
 
         auto& callouts = src.callouts()->callouts();
         ASSERT_EQ(callouts.size(), 2);
