d988cee1d4b9f43425ccc96209d163e38aa65f5c
Adriana Kobylak <anoo@us.ibm.com>
2025-08-01 15:27:45 -0500

    Audit Log: plugin: Add callback function
    Add a callback function to handle the audit event record that audispd
    sends, based on the example provided by the audisp repo:
    ```
    https://github.com/linux-audit/audit-userspace/blob/master/contrib/plugin/audisp-example.c
    ```
    
    Texted: Generated an audit event to trigger the plugin callback:
    ```
    Aug 01 19:59:05 p11bmc phosphor-auditlog-plugin[1583]: Handling audit
    event
    ```
    
    Change-Id: If6ddf14236c25b259e258a40b6cefa38e4897f69
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- phosphor-auditlog/main_plugin.cpp
+++ phosphor-auditlog/main_plugin.cpp
@@ -1,8 +1,53 @@
+#include <auparse.h>
+#include <libaudit.h>
+#include <unistd.h>
+
 #include <phosphor-logging/lg2.hpp>
+
+static void handleEvent(auparse_state_t* /*au*/,
+                        auparse_cb_event_t /*eventType*/, void* /*data*/)
+{
+    lg2::info("Handling audit event");
+}
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-    lg2::info("An audit event was received");
+    char buf[MAX_AUDIT_MESSAGE_LENGTH];
+
+    auparse_state_t* au = NULL;
+    au = auparse_init(AUSOURCE_FEED, 0);
+    if (au == nullptr)
+    {
+        lg2::error("Failed to init auparse feed");
+        return 1;
+    }
+
+    auparse_add_callback(au, handleEvent, NULL, NULL);
+
+    do
+    {
+        auto r = read(STDIN_FILENO, buf, sizeof(buf));
+        if (r == 0)
+        {
+            break;
+        }
+        else if (r < 0)
+        {
+            lg2::error("Error reading from stdin: errno = {ERRNO}", "ERRNO",
+                       errno);
+            break;
+        }
+
+        // Send data to parser
+        auparse_feed(au, buf, r);
+
+        // Flush events from queue
+        auparse_flush_feed(au);
+
+    } while (1);
+
+    auparse_flush_feed(au);
+    auparse_destroy(au);
 
     return 0;
 }
